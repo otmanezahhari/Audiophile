a8babe3a9318aeaa5e7fc55c9b9eac5bab7d2656
zahhari <zahhari.otmane@gmail.com>
2021-07-29 06:44:43 +0200

    add Redux + Structure folder of projet

---
+++ src/components/Categories/index.js
@@ -0,0 +1,51 @@
+import {Link } from "react-router-dom";
+import {scrollToTop} from "components/Healpers";
+import "./categories.scss";
+
+
+function Categories(props){
+  const ItemCategorie = () =>{
+    const Items = [
+      {
+        name :'headphones',
+      },
+      {
+        name :'speakers',
+      },
+      {
+        name :'earphones',
+      },
+  ]
+
+
+  return(
+    Items.map((elem,index) => {
+      return(
+        <Link key={index} to = {"/" + elem.name}  className="categorie-item" onClick={scrollToTop}>
+            <div className={`box-img-categorie ${elem.name}`}>
+              <img src={"/Audiophile/assets/shared/desktop/image-" + elem.name + ".png"} alt={elem.name}/>
+            </div>
+            <div className="description-categorie">
+              <h3 className ="cart-title" > {elem.name}</h3>
+              <div  className="link-categorie center">
+                <p className="shop">shop</p>
+                <img src={"/Audiophile/assets/shared/desktop/icon-arrow-right.svg"} alt="shop" />
+              </div>
+            </div>
+        </Link>
+      )
+    })
+  )
+  
+  }
+  
+  return(
+    <div className ="categories-items-gnrl">
+      <div className="categorie-items flex center container justify-content-between">
+        <ItemCategorie/>
+      </div> 
+    </div>
+  )
+}
+
+export default Categories;
